2bac9948b616dfcabd44d6143764cabdb4138224
michaelaboah <maboah@PlexusParc.com>
2023-03-19 19:53:11 -0400

    Refactored Video Compnents

--- src/components/Meeting/MeetingContainer.vue
+++ src/components/Meeting/MeetingContainer.vue
@@ -1,22 +1,51 @@
 <script setup lang="ts">
-    import { ref } from "vue";
-    import { StunServers } from "../../@types/RTCComponents"
-    import RemoteFrame from "../Video/RemoteFrame.vue";
-    import UserFrame from "../Video/UserFrame.vue";
+    import { ref, onMounted, reactive } from "vue";
+    import Frame from "../Video/Frame.vue"
     import Chat from "./Chat.vue";
 
-    const participants = ref([1, 2]);
+    type Participant = {
+        username: string,
+        camStream: MediaStream,
+        isHost: boolean,
+        
+    }
+
+    const videoElement = ref<HTMLVideoElement | null>(null);
     const stunServers = ref<RTCConfiguration>({
-            iceServers: [
-                { 
-                    urls: [
-                        "stun:stun.plexicomm.net:3478",
-                    ]
-                }
-            ]
+        iceServers: [
+            { 
+                urls: [
+                    "stun:stun.plexicomm.net:3478",
+                ]
+            }
+        ]
     })
 
+    
+    let localStream = ref<MediaStream>()
 
+
+
+    const participants = reactive([
+
+    ])
+
+    async function init(){
+        try {
+            localStream.value = await navigator.mediaDevices.getUserMedia({video: true, audio: false})     
+            console.log(localStream.value)      
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    onMounted( () => {
+        init().then(() => {
+            if (localStream.value) {
+                videoElement.value!.srcObject = localStream.value
+            }
+        })
+    })
 
 
 
@@ -25,9 +54,9 @@
 <template>
     <div class="container flex items-center gap-3">
         <!-- <div  :key="index" v-for="(p, index) in participants"> -->
-            <UserFrame/>
-            <RemoteFrame :configuration="stunServers"/>
+            <Frame v-if="localStream" :stream="localStream"/>
         <!-- </div> -->
+
         <Chat/>
     </div>
 </template>
---
+++ src/components/Video/Frame.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    import {  onMounted, ref } from 'vue';
+
+    const props = defineProps({
+        stream: {
+            type: MediaStream,
+            required: true
+        }
+    })
+
+    let videoElement = ref<HTMLVideoElement | null>(null)
+
+    onMounted(() => {
+        if (props.stream) {
+            console.log(props.stream)
+            videoElement.value!.srcObject = props.stream
+        }
+    })
+    
+</script>
+
+<template>
+    <div class="h-full">
+        <video  @pause.prevent
+                @mouseenter.stop
+                @mouseenter.prevent
+             class="object-none" ref="videoElement" preload="none"  autoplay playsinline="true"/>
+    </div>
+</template>
+
+<style scoped>
+    /* video::-webkit-media-controls-start-playback-button {
+        display: none;
+    } */
+</style>
